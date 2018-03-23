a4bed7d3d7e2810d9373a3cca8a87f6a886a747a
sbinda <sebastien.binda@c-s.fr>
2018-03-23 17:09:12 +0100

    Fix plugin standalone compilation.

--- datasource-plugins/db-datasource-common/src/main/java/fr/cnes/regards/db/datasources/plugins/common/AbstractDBConnection.java
+++ datasource-plugins/db-datasource-common/src/main/java/fr/cnes/regards/db/datasources/plugins/common/AbstractDBConnection.java
@@ -17,7 +17,7 @@
  * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package fr.cnes.regards.modules.datasources.utils;
+package fr.cnes.regards.db.datasources.plugins.common;
 
 import java.beans.PropertyVetoException;
 import java.sql.Connection;
