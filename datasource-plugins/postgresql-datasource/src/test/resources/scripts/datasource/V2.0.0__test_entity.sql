b85d0f2befc6fee41eeee989a697da46fd83d60f
Marc Sordi <marc.sordi@c-s.fr>
2018-08-07 16:37:05 +0200

    add missing descr attribute

--- datasource-plugins/postgresql-datasource/src/test/resources/scripts/datasource/V2.0.0__test_entity.sql
+++ datasource-plugins/postgresql-datasource/src/test/resources/scripts/datasource/V2.0.0__test_entity.sql
@@ -10,7 +10,8 @@ create table t_test_plugin_data_source (
     timewithouttimezone time,
     update boolean,
     dateStr varchar(255),
-    url text
+    url text,
+    descr varchar(255)
 );
 create index index_test on t_test_plugin_data_source (altitude);
 create sequence seq_test_plugin start 1 increment 50;
