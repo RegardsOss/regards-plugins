b086d6bc7f2248f59c3825ab076fc98cea7bde84
sbinda <sebastien.binda@c-s.fr>
2019-02-22 15:53:49 +0100

    Fix db access for tests.

--- datasource-plugins/webservice-datasource/src/test/java/fr/cnes/regards/modules/dam/plugins/datasources/webservice/WebserviceDatasourcePluginTest.java
+++ datasource-plugins/webservice-datasource/src/test/java/fr/cnes/regards/modules/dam/plugins/datasources/webservice/WebserviceDatasourcePluginTest.java
@@ -1,9 +1,19 @@
 package fr.cnes.regards.modules.dam.plugins.datasources.webservice;
 
+import java.util.HashMap;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.TestPropertySource;
+
 import com.google.gson.Gson;
+
 import fr.cnes.httpclient.HttpClient;
+import fr.cnes.regards.framework.jpa.utils.RegardsTransactional;
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
-import fr.cnes.regards.framework.test.integration.AbstractRegardsServiceIT;
+import fr.cnes.regards.framework.test.integration.AbstractRegardsServiceTransactionalIT;
 import fr.cnes.regards.modules.dam.domain.datasources.plugins.DataSourceException;
 import fr.cnes.regards.modules.dam.domain.entities.StaticProperties;
 import fr.cnes.regards.modules.dam.domain.entities.feature.DataObjectFeature;
@@ -11,22 +21,11 @@ import fr.cnes.regards.modules.dam.plugins.datasources.webservice.configuration.
 import fr.cnes.regards.modules.dam.plugins.datasources.webservice.configuration.WebserviceConfiguration;
 import fr.cnes.regards.modules.dam.service.models.IModelAttrAssocService;
 import fr.cnes.regards.modules.notification.client.INotificationClient;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.time.OffsetDateTime;
-import java.util.HashMap;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@TestPropertySource(locations = {"classpath:test.properties"})
-public class WebserviceDatasourcePluginTest extends AbstractRegardsServiceIT {
+@TestPropertySource(locations = { "classpath:test.properties" },
+        properties = { "spring.jpa.properties.hibernate.default_schema=public" })
+@RegardsTransactional
+public class WebserviceDatasourcePluginTest extends AbstractRegardsServiceTransactionalIT {
 
     @Autowired
     private IModelAttrAssocService modelAttrAssocService;
@@ -41,7 +40,7 @@ public class WebserviceDatasourcePluginTest extends AbstractRegardsServiceIT {
     private Gson gson;
 
     @Test
-    public void doNothing(){
+    public void doNothing() {
         // TODO delete this method (only for maven to compile with tests, temporary)
     }
 
@@ -54,13 +53,15 @@ public class WebserviceDatasourcePluginTest extends AbstractRegardsServiceIT {
         attributeToJSonField.put(StaticProperties.FEATURE_LABEL, "title");
         attributeToJSonField.put(StaticProperties.FEATURE_PROVIDER_ID, "productIdentifier");
         WebserviceDatasourcePlugin pl = new WebserviceDatasourcePlugin(
-                new WebserviceConfiguration("https://theia.cnes.fr/atdistrib/resto2/api/collections/LANDSAT/search.json", "page", "maxRecords", null, 1, 20),
-                new ConversionConfiguration("PATATO", attributeToJSonField, null, null, null, "totalResults", "itemsPerPage"), 90000,
-                modelAttrAssocService, notificationClient, httpClient, gson);
+                new WebserviceConfiguration(
+                        "https://theia.cnes.fr/atdistrib/resto2/api/collections/LANDSAT/search.json", "page",
+                        "maxRecords", null, 1, 20),
+                new ConversionConfiguration("PATATO", attributeToJSonField, null, null, null, "totalResults",
+                        "itemsPerPage"),
+                90000, modelAttrAssocService, notificationClient, httpClient, gson);
         pl.initialize();
         // TODO: expected results!
         Page<DataObjectFeature> convertedPage = pl.findAll("testTenant", PageRequest.of(0, 20));
     }
-
 
 }
