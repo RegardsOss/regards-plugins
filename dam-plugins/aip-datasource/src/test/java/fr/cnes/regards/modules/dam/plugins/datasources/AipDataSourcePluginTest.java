1e05fb46469907112c5bc1ce369bf3d7089d97a3
Simon MILHAU <simon.milhau@outlook.com>
2019-10-10 11:04:51 +0200

    Change the AipDataSourcePlugin to use directly ingest instead of storage

--- dam-plugins/aip-datasource/src/test/java/fr/cnes/regards/modules/dam/plugins/datasources/AipDataSourcePluginTest.java
+++ dam-plugins/aip-datasource/src/test/java/fr/cnes/regards/modules/dam/plugins/datasources/AipDataSourcePluginTest.java
@@ -23,23 +23,29 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import com.google.gson.Gson;
+import fr.cnes.regards.framework.gson.adapters.OffsetDateTimeAdapter;
+import fr.cnes.regards.framework.oais.urn.EntityType;
+import fr.cnes.regards.framework.oais.urn.OAISIdentifier;
+import fr.cnes.regards.framework.oais.urn.UniformResourceName;
+import fr.cnes.regards.modules.ingest.dto.aip.AIP;
+import fr.cnes.regards.modules.ingest.dto.aip.AIPBuilder;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
 import com.google.common.collect.Lists;
-import com.google.gson.JsonElement;
 
 import fr.cnes.regards.framework.module.rest.exception.ModuleException;
 import fr.cnes.regards.framework.modules.plugins.domain.parameter.IPluginParam;
@@ -64,9 +70,10 @@ import fr.cnes.regards.modules.dam.service.models.IModelService;
  *
  * @author oroussel
  */
-@Ignore("TODO !!!!")
 @TestPropertySource("classpath:aip-datasource-test.properties")
+@ContextConfiguration(classes = { AipDataSourcePluginTestConfiguration.class })
 public class AipDataSourcePluginTest extends AbstractRegardsServiceIT {
+
 
     private static final String MODEL_FILE_NAME = "model.xml";
 
@@ -82,6 +89,7 @@ public class AipDataSourcePluginTest extends AbstractRegardsServiceIT {
 
     @Before
     public void setUp() throws SQLException, ModuleException, NotAvailablePluginConfigurationException {
+        PluginUtils.setup();
         tenantResolver.forceTenant(getDefaultTenant());
         try {
             // Remove the model if existing
@@ -96,7 +104,7 @@ public class AipDataSourcePluginTest extends AbstractRegardsServiceIT {
 
         // Instantiate the data source plugin
         Set<IPluginParam> parameters = IPluginParam
-                .set(IPluginParam.build(DataSourcePluginConstants.BINDING_MAP, createBindingMap()),
+                .set(IPluginParam.build(DataSourcePluginConstants.BINDING_MAP, PluginParameterTransformer.toJson(createBindingMap())),
                      IPluginParam.build(DataSourcePluginConstants.SUBSETTING_TAGS,
                                         PluginParameterTransformer.toJson(Arrays.asList(MODEL_NAME))),
                      IPluginParam.build(DataSourcePluginConstants.MODEL_NAME_PARAM, MODEL_NAME),
@@ -124,16 +132,12 @@ public class AipDataSourcePluginTest extends AbstractRegardsServiceIT {
         }
     }
 
-    // This method is called by Aip client proxy (from AipDataSourceConfiguration) to provide some AIPs when calling
-    // aip client method
-    /**
-     * TODO : Use new storage and Ingest manager
     protected static List<AIP> createAIPs(int count, String... tags) {
         List<AIP> aips = new ArrayList<>();
         for (int i = 0; i < count; i++) {
             UniformResourceName id = new UniformResourceName(OAISIdentifier.AIP, EntityType.DATA, "TENANT",
                     UUID.randomUUID(), 1);
-            AIPBuilder builder = new AIPBuilder(id, Optional.empty(), "sipId" + i, EntityType.DATA, "session 1");
+            AIPBuilder builder = new AIPBuilder(id, Optional.empty(), "sipId" + i, EntityType.DATA);
             builder.addTags(tags);
 
             builder.addDescriptiveInformation("label", "libellé du data object " + i);
@@ -146,7 +150,6 @@ public class AipDataSourcePluginTest extends AbstractRegardsServiceIT {
                     .asList("paris", "toulouse", "lyon", "nice", "bordeaux").stream().collect(Collectors.toList()));
             builder.addDescriptiveInformation("POUET", "POUET");
             builder.addDescriptiveInformation("LINKS", "https://sipad-serad.cnes.fr");
-
             Map<String, String> dateBounds = new HashMap<>();
             dateBounds.put(DataSourcePluginConstants.LOWER_BOUND, OffsetDateTime.now().atZoneSameInstant(ZoneOffset.UTC)
                     .format(OffsetDateTimeAdapter.ISO_DATE_TIME_UTC));
@@ -158,46 +161,36 @@ public class AipDataSourcePluginTest extends AbstractRegardsServiceIT {
             intBounds.put("ilow", 100);
             intBounds.put("iup", null);
             builder.addDescriptiveInformation("intrange", intBounds);
-
             aips.add(builder.build());
         }
         return aips;
     }
-    */
 
     /**
      * Binding map from AIP (key) properties and associated model attributes (value)
      */
-    private Map<String, JsonElement> createBindingMap() {
-        HashMap<String, JsonElement> map = new HashMap<>();
-        map.put("label", PluginParameterTransformer.toJson("properties.descriptiveInformation.label"));
-        map.put("properties.START_DATE",
-                PluginParameterTransformer.toJson("properties.descriptiveInformation.START_DATE"));
-        map.put("properties.ALTITUDE.MAX",
-                PluginParameterTransformer.toJson("properties.descriptiveInformation.ALT_MAX"));
-        map.put("properties.ALTITUDE.MIN",
-                PluginParameterTransformer.toJson("properties.descriptiveInformation.ALT_MIN"));
-        map.put("properties.history", PluginParameterTransformer.toJson("properties.descriptiveInformation.HISTORY"));
-        map.put("properties.history_set",
-                PluginParameterTransformer.toJson("properties.descriptiveInformation.HISTORY_SET"));
-        map.put("properties.history_list",
-                PluginParameterTransformer.toJson("properties.descriptiveInformation.HISTORY_LIST"));
-        map.put("properties.LINKS", PluginParameterTransformer.toJson("properties.descriptiveInformation.LINKS"));
+    private Map<String, String> createBindingMap() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("label", "properties.descriptiveInformation.label");
+        map.put("properties.START_DATE", "properties.descriptiveInformation.START_DATE");
+        map.put("properties.ALTITUDE.MAX", "properties.descriptiveInformation.ALT_MAX");
+        map.put("properties.ALTITUDE.MIN", "properties.descriptiveInformation.ALT_MIN");
+        map.put("properties.history", "properties.descriptiveInformation.HISTORY");
+        map.put("properties.history_set", "properties.descriptiveInformation.HISTORY_SET");
+        map.put("properties.history_list", "properties.descriptiveInformation.HISTORY_LIST");
+        map.put("properties.LINKS", "properties.descriptiveInformation.LINKS");
 
         // Date interval
-        map.put("properties.DATE_INTERVAL" + DataSourcePluginConstants.LOWER_BOUND_SUFFIX, PluginParameterTransformer
-                .toJson("properties.descriptiveInformation.range" + DataSourcePluginConstants.LOWER_BOUND_SUFFIX));
-        map.put("properties.DATE_INTERVAL" + DataSourcePluginConstants.UPPER_BOUND_SUFFIX, PluginParameterTransformer
-                .toJson("properties.descriptiveInformation.range" + DataSourcePluginConstants.UPPER_BOUND_SUFFIX));
+        map.put("properties.DATE_INTERVAL" + DataSourcePluginConstants.LOWER_BOUND_SUFFIX,
+                "properties.descriptiveInformation.range" + DataSourcePluginConstants.LOWER_BOUND_SUFFIX);
+        map.put("properties.DATE_INTERVAL" + DataSourcePluginConstants.UPPER_BOUND_SUFFIX,
+                "properties.descriptiveInformation.range" + DataSourcePluginConstants.UPPER_BOUND_SUFFIX);
 
         // Integer open interval
         map.put("properties.INT_INTERVAL" + DataSourcePluginConstants.LOWER_BOUND_SUFFIX,
-                PluginParameterTransformer.toJson("properties.descriptiveInformation.intrange.ilow"));
+                "properties.descriptiveInformation.intrange.ilow");
         map.put("properties.INT_INTERVAL" + DataSourcePluginConstants.UPPER_BOUND_SUFFIX,
-                PluginParameterTransformer.toJson("properties.descriptiveInformation.intrange.iup"));
-
-        // FIXME
-        // map.put("properties.history", "properties.descriptiveInformation.NIMP");
+                "properties.descriptiveInformation.intrange.iup");
         return map;
     }
 
@@ -206,7 +199,8 @@ public class AipDataSourcePluginTest extends AbstractRegardsServiceIT {
         Page<DataObjectFeature> page = dsPlugin.findAll(getDefaultTenant(), PageRequest.of(0, 10));
         Assert.assertNotNull(page);
         Assert.assertNotNull(page.getContent());
-        Assert.assertTrue(page.getContent().size() > 0);
+        Assert.assertTrue(page.getContent().size() == 1);
+
         DataObjectFeature feature = page.getContent().get(0);
         Assert.assertEquals("libellé du data object 0", feature.getLabel());
         Assert.assertNotNull(feature.getProperty("START_DATE"));
@@ -232,6 +226,8 @@ public class AipDataSourcePluginTest extends AbstractRegardsServiceIT {
 
         Assert.assertTrue(feature.getProperty("SIZE") instanceof LongAttribute);
 
+        Assert.assertTrue(feature.getFiles().get(DataType.RAWDATA).iterator().next().isOnline());
+        Assert.assertFalse(feature.getFiles().get(DataType.RAWDATA).iterator().next().isReference());
     }
 
     @After
