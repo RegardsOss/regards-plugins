e901c33d857be86fb57b4c7e9e82d79c2dd03d66
sbinda <sebastien.binda@c-s.fr>
2020-04-06 14:28:57 +0200

    fix tests

--- catalog-plugins/fem-driver/fem-driver-service/src/test/java/fr/cnes/regards/modules/catalog/femdriver/service/job/FemUpdateJobTest.java
+++ catalog-plugins/fem-driver/fem-driver-service/src/test/java/fr/cnes/regards/modules/catalog/femdriver/service/job/FemUpdateJobTest.java
@@ -50,7 +50,7 @@ import fr.cnes.regards.modules.search.domain.plugin.SearchEngineMappings;
  * @author Sébastien Binda
  *
  */
-@TestPropertySource(locations = { "classpath:test.properties", "classpath:local.properties" },
+@TestPropertySource(locations = { "classpath:test.properties" },
         properties = { "spring.jpa.properties.hibernate.default_schema=fem_job" })
 public class FemUpdateJobTest extends AbstractFemJobTest {
 
