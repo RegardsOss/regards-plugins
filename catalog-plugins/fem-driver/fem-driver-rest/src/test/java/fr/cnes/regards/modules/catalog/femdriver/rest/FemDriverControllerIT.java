a5d0d3e0be2083ca622e3ffad4cd1bb98fa36019
sbinda <sebastien.binda@c-s.fr>
2020-04-08 09:52:49 +0200

    FEM DRIVER : Add GsonAdpater for FeatureUpdateRequest. Add Rest tests IT

---
+++ catalog-plugins/fem-driver/fem-driver-rest/src/test/java/fr/cnes/regards/modules/catalog/femdriver/rest/FemDriverControllerIT.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2017-2019 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.regards.modules.catalog.femdriver.rest;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.util.LinkedMultiValueMap;
+
+import com.google.common.collect.Sets;
+
+import fr.cnes.regards.framework.module.rest.exception.ModuleException;
+import fr.cnes.regards.framework.test.integration.RequestBuilderCustomizer;
+import fr.cnes.regards.modules.catalog.femdriver.dto.FeatureUpdateRequest;
+import fr.cnes.regards.modules.model.domain.attributes.AttributeModel;
+import fr.cnes.regards.modules.model.dto.properties.IProperty;
+import fr.cnes.regards.modules.search.domain.SearchRequest;
+import fr.cnes.regards.modules.search.domain.plugin.SearchEngineMappings;
+
+/**
+ * @author sbinda
+ *
+ */
+@TestPropertySource(locations = { "classpath:test.properties" },
+        properties = { "spring.jpa.properties.hibernate.default_schema=femdriver_rest_it" })
+public class FemDriverControllerIT extends AbstractFemTest {
+
+    @Before
+    public void init() throws ModuleException {
+        this.doInit();
+    }
+
+    @Test
+    public void update() {
+        List<AttributeModel> atts = attributeModelService.getAttributes(null, null, null);
+        gsonAttributeFactory.refresh(getDefaultTenant(), atts);
+        Set<IProperty<?>> properties = Sets.newHashSet();
+        properties.add(IProperty.buildString("name", "plop"));
+        SearchRequest searchRequest = new SearchRequest(SearchEngineMappings.LEGACY_PLUGIN_ID, null,
+                new LinkedMultiValueMap<String, String>(), null, null, null);
+        FeatureUpdateRequest req = FeatureUpdateRequest.build(searchRequest, properties);
+
+        RequestBuilderCustomizer customizer = customizer();
+        customizer.expect(MockMvcResultMatchers.status().isNoContent());
+        performDefaultPost(FemDriverController.FEM_DRIVER_PATH + FemDriverController.FEM_DRIVER_UPDATE_PATH, req,
+                           customizer, "Failed to create a new dataset");
+
+    }
+
+}
---
+++ catalog-plugins/fem-driver/fem-driver-rest/src/test/java/fr/cnes/regards/modules/catalog/femdriver/rest/TestConfiguration.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2017-2019 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.regards.modules.catalog.femdriver.rest;
+
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+
+import fr.cnes.regards.framework.hateoas.IResourceService;
+import fr.cnes.regards.modules.accessrights.client.IProjectUsersClient;
+import fr.cnes.regards.modules.dam.client.dataaccess.IAccessGroupClient;
+import fr.cnes.regards.modules.dam.client.dataaccess.IUserClient;
+import fr.cnes.regards.modules.dam.client.entities.IDatasetClient;
+import fr.cnes.regards.modules.model.client.IAttributeModelClient;
+import fr.cnes.regards.modules.model.client.IModelAttrAssocClient;
+import fr.cnes.regards.modules.project.client.rest.IProjectsClient;
+import fr.cnes.regards.modules.project.domain.Project;
+import fr.cnes.regards.modules.storage.client.IStorageRestClient;
+
+/**
+ * @author sbinda
+ *
+ */
+@Configuration
+public class TestConfiguration {
+
+    @Bean
+    public IProjectsClient projectsClient() {
+        IProjectsClient projectsClientMock = Mockito.mock(IProjectsClient.class);
+        // Manage project
+        Project project = new Project(1L, "Solar system project", "http://plop/icon.png", true, "SolarSystem");
+        project.setHost("http://regards/solarsystem");
+        ResponseEntity<EntityModel<Project>> response = ResponseEntity.ok(new EntityModel<>(project));
+        Mockito.when(projectsClientMock.retrieveProject(Mockito.anyString())).thenReturn(response);
+        return projectsClientMock;
+    }
+
+    @Bean
+    public IAttributeModelClient attClient() {
+        return Mockito.mock(IAttributeModelClient.class);
+    }
+
+    @Bean
+    public IModelAttrAssocClient attAssocClient() {
+        return Mockito.mock(IModelAttrAssocClient.class);
+    }
+
+    @Bean
+    public IDatasetClient dataSetClient() {
+        return Mockito.mock(IDatasetClient.class);
+    }
+
+    @Bean
+    public IUserClient userClient() {
+        return Mockito.mock(IUserClient.class);
+    }
+
+    @Bean
+    public IAccessGroupClient accessGroupClient() {
+        return Mockito.mock(IAccessGroupClient.class);
+    }
+
+    @Bean
+    public IProjectUsersClient projectUsersClient() {
+        return Mockito.mock(IProjectUsersClient.class);
+    }
+
+    @Bean
+    public IResourceService resourceServiceClient() {
+        return Mockito.mock(IResourceService.class);
+    }
+
+    @Bean
+    @Primary
+    public IStorageRestClient storageRestClient() {
+        return Mockito.mock(IStorageRestClient.class);
+    }
+
+}
