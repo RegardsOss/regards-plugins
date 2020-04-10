e6fcfdc201f73a350b8873694ebf6a77e6a21803
sbinda <sebastien.binda@c-s.fr>
2020-04-10 19:22:11 +0200

    Add new version of FeatureFacctory Plugin.

---
+++ fem-plugins/feature-factory/src/main/java/fr/cnes/regards/modules/fem/plugins/FeatureFactoryPlugin2.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017-2020 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.regards.modules.fem.plugins;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.cnes.regards.framework.module.rest.exception.ModuleException;
+import fr.cnes.regards.framework.modules.plugins.annotations.Plugin;
+import fr.cnes.regards.modules.feature.domain.plugin.IFeatureFactoryPlugin;
+import fr.cnes.regards.modules.feature.domain.request.FeatureReferenceRequest;
+import fr.cnes.regards.modules.feature.dto.Feature;
+import fr.cnes.regards.modules.fem.plugins.service2.FeatureFactoryService;
+
+/**
+ * Create a {@link Feature} from a {@link FeatureReferenceRequest}
+ * We will use the file name to extract {@link Feature} metadata
+ * @author Kevin Marchois
+ *
+ */
+@Plugin(author = "REGARDS Team", description = "Create a feature from a file reference", id = "FeatureFactoryPlugin2",
+        version = "1.0.0", contact = "regards@c-s.fr", license = "GPLv3", owner = "CNES",
+        url = "https://regardsoss.github.io/")
+public class FeatureFactoryPlugin2 implements IFeatureFactoryPlugin {
+
+    public static final String MODEL = "model";
+
+    @Autowired
+    private FeatureFactoryService factoryService;
+
+    @Override
+    public Feature createFeature(FeatureReferenceRequest reference) throws ModuleException {
+        return factoryService.getFeature(reference.getLocation(), MODEL);
+    }
+
+}
