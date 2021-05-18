b3adc39ceb060d5c7573bab5da796193d7cf4b4c
Marc Sordi <marc.sordi@csgroup.eu>
2021-05-18 11:16:59 +0200

    [STAC] Fix property extraction

--- catalog-plugins/stac/stac-service/src/main/java/fr/cnes/regards/modules/catalog/stac/service/item/RegardsFeatureToStacItemConverterImpl.java
+++ catalog-plugins/stac/stac-service/src/main/java/fr/cnes/regards/modules/catalog/stac/service/item/RegardsFeatureToStacItemConverterImpl.java
@@ -19,6 +19,20 @@
 
 package fr.cnes.regards.modules.catalog.stac.service.item;
 
+import static fr.cnes.regards.modules.catalog.stac.domain.error.StacFailureType.DATAOBJECT_TO_ITEM_CONVERSION;
+import static fr.cnes.regards.modules.catalog.stac.domain.error.StacRequestCorrelationId.debug;
+import static fr.cnes.regards.modules.catalog.stac.domain.error.StacRequestCorrelationId.warn;
+import static fr.cnes.regards.modules.catalog.stac.domain.utils.TryDSL.trying;
+import static io.vavr.Predicates.isNotNull;
+import static java.lang.String.format;
+
+import java.net.URI;
+
+import org.locationtech.spatial4j.io.GeoJSONReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
 import fr.cnes.regards.framework.geojson.geometry.IGeometry;
 import fr.cnes.regards.framework.urn.DataType;
 import fr.cnes.regards.modules.catalog.stac.domain.properties.StacProperty;
@@ -35,26 +49,17 @@ import fr.cnes.regards.modules.catalog.stac.service.link.StacLinkCreator;
 import fr.cnes.regards.modules.catalog.stac.service.link.UriParamAdder;
 import fr.cnes.regards.modules.dam.domain.entities.DataObject;
 import fr.cnes.regards.modules.indexer.domain.DataFile;
-import fr.cnes.regards.modules.model.dto.properties.IProperty;
 import io.vavr.Tuple;
 import io.vavr.Tuple2;
 import io.vavr.Tuple3;
-import io.vavr.collection.*;
+import io.vavr.collection.HashMap;
+import io.vavr.collection.HashSet;
+import io.vavr.collection.List;
+import io.vavr.collection.Map;
+import io.vavr.collection.Set;
+import io.vavr.collection.Stream;
 import io.vavr.control.Option;
 import io.vavr.control.Try;
-import org.locationtech.spatial4j.io.GeoJSONReader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-
-import java.net.URI;
-
-import static fr.cnes.regards.modules.catalog.stac.domain.error.StacFailureType.DATAOBJECT_TO_ITEM_CONVERSION;
-import static fr.cnes.regards.modules.catalog.stac.domain.error.StacRequestCorrelationId.debug;
-import static fr.cnes.regards.modules.catalog.stac.domain.error.StacRequestCorrelationId.warn;
-import static fr.cnes.regards.modules.catalog.stac.domain.utils.TryDSL.trying;
-import static io.vavr.Predicates.isNotNull;
-import static java.lang.String.format;
 
 /**
  * Default implementation for {@link RegardsFeatureToStacItemConverter} interface.
@@ -63,51 +68,39 @@ import static java.lang.String.format;
 public class RegardsFeatureToStacItemConverterImpl implements RegardsFeatureToStacItemConverter, StacLinkCreator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RegardsFeatureToStacItemConverterImpl.class);
-    
+
     private final StacGeoHelper geoHelper;
+
     private final ConfigurationAccessorFactory configurationAccessorFactory;
+
     private final UriParamAdder uriParamAdder;
 
-    public RegardsFeatureToStacItemConverterImpl(
-            StacGeoHelper geoHelper,
-            ConfigurationAccessorFactory configurationAccessorFactory,
-            UriParamAdder uriParamAdder
-    ) {
+    public RegardsFeatureToStacItemConverterImpl(StacGeoHelper geoHelper,
+            ConfigurationAccessorFactory configurationAccessorFactory, UriParamAdder uriParamAdder) {
         this.geoHelper = geoHelper;
         this.configurationAccessorFactory = configurationAccessorFactory;
         this.uriParamAdder = uriParamAdder;
     }
 
     @Override
-    public Try<Item> convertFeatureToItem(List<StacProperty> properties, OGCFeatLinkCreator linkCreator, DataObject feature) {
+    public Try<Item> convertFeatureToItem(List<StacProperty> properties, OGCFeatLinkCreator linkCreator,
+            DataObject feature) {
         debug(LOGGER, "Converting to item: Feature={}\n\twith Properties={}", feature, properties);
         return trying(() -> {
             ConfigurationAccessor configurationAccessor = configurationAccessorFactory.makeConfigurationAccessor();
-            Map<String,Object> featureStacProperties = extractStacPropertyKeyValues(feature, properties);
+            Map<String, Object> featureStacProperties = extractStacProperties(feature, properties);
             Set<String> extensions = extractExtensions(featureStacProperties);
             Tuple3<IGeometry, BBox, Centroid> geo = extractGeo(feature, configurationAccessor.getGeoJSONReader());
             String collection = extractCollection(feature).getOrNull();
             String itemId = feature.getIpId().toString();
             Tuple2<String, String> authParam = uriParamAdder.makeAuthParam();
 
-            Item result = new Item(
-                    extensions,
-                    itemId,
-                    geo._2,
-                    geo._1,
-                    geo._3,
-                    collection,
-                    featureStacProperties,
-                    extractLinks(itemId, collection, linkCreator),
-                    extractAssets(feature, authParam)
-            );
+            Item result = new Item(extensions, itemId, geo._2, geo._1, geo._3, collection, featureStacProperties,
+                    extractLinks(itemId, collection, linkCreator), extractAssets(feature, authParam));
             debug(LOGGER, "Result Item={}", result);
             return result;
-        })
-        .mapFailure(
-                DATAOBJECT_TO_ITEM_CONVERSION,
-            () -> format("Failed to convert data object %s to item", feature.getIpId())
-        );
+        }).mapFailure(DATAOBJECT_TO_ITEM_CONVERSION,
+                      () -> format("Failed to convert data object %s to item", feature.getIpId()));
     }
 
     private Map<String, Asset> extractAssets(DataObject feature, Tuple2<String, String> authParam) {
@@ -118,43 +111,32 @@ public class RegardsFeatureToStacItemConverterImpl implements RegardsFeatureToSt
     }
 
     private Tuple2<String, Asset> extractAsset(DataFile value, Tuple2<String, String> authParam) {
-        Tuple2<String, Asset> result = Tuple.of(
-            value.getFilename(),
-            new Asset(
-                authdUri(value.asUri(), authParam),
-                value.getFilename(),
-                format("File size: %d bytes" +
-                    "\n\nIs reference: %b" +
-                    "\n\nIs online: %b" +
-                    "\n\nDatatype: %s" +
-                    "\n\nChecksum %s: %s",
-                    value.getFilesize(),
-                    value.isReference(),
-                    value.isOnline(),
-                    value.getDataType(),
-                    value.getDigestAlgorithm(), value.getChecksum()
-                ),
-                value.getMimeType().toString(),
-                HashSet.of(assetTypeFromDatatype(value.getDataType()))
-            )
-        );
+        Tuple2<String, Asset> result = Tuple
+                .of(value.getFilename(),
+                    new Asset(authdUri(value.asUri(), authParam), value.getFilename(),
+                            format("File size: %d bytes" + "\n\nIs reference: %b" + "\n\nIs online: %b" + "\n\nDatatype: %s"
+                                    + "\n\nChecksum %s: %s",
+                                   value.getFilesize(), value.isReference(), value.isOnline(), value.getDataType(),
+                                   value.getDigestAlgorithm(), value.getChecksum()),
+                            value.getMimeType().toString(), HashSet.of(assetTypeFromDatatype(value.getDataType()))));
         debug(LOGGER, "Found asset: \n\tDataFile={} ; \n\tAsset={}", value.getChecksum(), result);
         return result;
     }
 
     private URI authdUri(URI uri, Tuple2<String, String> authParam) {
-        return Try.success(uri)
-            .flatMapTry(uriParamAdder.appendParams(HashMap.of(authParam)))
-            .getOrElse(uri);
+        return Try.success(uri).flatMapTry(uriParamAdder.appendParams(HashMap.of(authParam))).getOrElse(uri);
     }
 
     private String assetTypeFromDatatype(DataType dataType) {
-        switch(dataType){
-            case QUICKLOOK_SD: case QUICKLOOK_MD: case QUICKLOOK_HD:
+        switch (dataType) {
+            case QUICKLOOK_SD:
+            case QUICKLOOK_MD:
+            case QUICKLOOK_HD:
                 return Asset.Roles.OVERVIEW;
             case THUMBNAIL:
                 return Asset.Roles.THUMBNAIL;
-            case DESCRIPTION: case DOCUMENT:
+            case DESCRIPTION:
+            case DOCUMENT:
                 return Asset.Roles.METADATA;
             default:
                 return Asset.Roles.DATA;
@@ -162,19 +144,14 @@ public class RegardsFeatureToStacItemConverterImpl implements RegardsFeatureToSt
     }
 
     private List<Link> extractLinks(String itemId, String collection, OGCFeatLinkCreator linkCreator) {
-        return List.of(
-            linkCreator.createRootLink(),
-            linkCreator.createCollectionLink(collection, "Item collection"),
-            linkCreator.createItemLink(collection, itemId)
-        )
-        .flatMap(tl -> tl);
+        return List.of(linkCreator.createRootLink(), linkCreator.createCollectionLink(collection, "Item collection"),
+                       linkCreator.createItemLink(collection, itemId))
+                .flatMap(tl -> tl);
     }
 
     private Option<String> extractCollection(DataObject feature) {
-        return HashSet.ofAll(feature.getTags())
-            .filter(tag -> tag.startsWith("URN:"))
-            .filter(tag -> tag.contains(":DATASET:"))
-            .headOption();
+        return HashSet.ofAll(feature.getTags()).filter(tag -> tag.startsWith("URN:"))
+                .filter(tag -> tag.contains(":DATASET:")).headOption();
     }
 
     private Tuple3<IGeometry, BBox, Centroid> extractGeo(DataObject feature, GeoJSONReader geoJSONReader) {
@@ -183,73 +160,81 @@ public class RegardsFeatureToStacItemConverterImpl implements RegardsFeatureToSt
         Option<BBox> bbox = Option.ofOptional(feature.getFeature().getBbox()).flatMap(this::extractBBox);
 
         return geometry
-            .flatMap(g -> bbox.map(b -> Tuple.of(g, b, b.centroid()))
-                .orElse(geoHelper.computeBBoxCentroid(g, geoJSONReader))
-            )
-            .orElse(bbox.map(bb -> Tuple.of(null, bb, bb.centroid())))
-            .getOrElse(() -> Tuple.of(null, null, null));
+                .flatMap(g -> bbox.map(b -> Tuple.of(g, b, b.centroid()))
+                        .orElse(geoHelper.computeBBoxCentroid(g, geoJSONReader)))
+                .orElse(bbox.map(bb -> Tuple.of(null, bb, bb.centroid()))).getOrElse(() -> Tuple.of(null, null, null));
     }
 
     private Option<BBox> extractBBox(Double[] doubles) {
         if (doubles.length == 4) {
             return Option.of(new BBox(doubles[0], doubles[1], doubles[2], doubles[3]));
-        }
-        else {
+        } else {
             return Option.none();
         }
     }
 
     private Set<String> extractExtensions(Map<String, Object> stacProperties) {
-        return stacProperties
-            .keySet()
-            .flatMap(name -> {
-                int colonIndex = name.indexOf(":");
-                return colonIndex == -1 ? Option.none() : Option.of(name.substring(0, colonIndex));
-            });
+        return stacProperties.keySet().flatMap(name -> {
+            int colonIndex = name.indexOf(":");
+            return colonIndex == -1 ? Option.none() : Option.of(name.substring(0, colonIndex));
+        });
     }
 
-    private Map<String, Object> extractStacPropertyKeyValues(DataObject feature, List<StacProperty> properties) {
-        return HashSet.ofAll(feature.getFeature().getProperties())
-            .map(regardsProp ->
-                findCorrespondingStacProperty(regardsProp, properties)
-                    .map(sp -> extractPropertyRegardsKeyValue(regardsProp, sp))
-                    .getOrElse(() -> Tuple.of(regardsProp.getName(), regardsProp.getValue()))
-            )
-            .toMap(kv -> kv)
-            .filterKeys(isNotNull());
+    private Map<String, Object> extractStacProperties(DataObject feature, List<StacProperty> stacProperties) {
+        return stacProperties.map(sp -> extractStacProperty(feature, sp)).toMap(kv -> kv);
     }
 
-    private Tuple2<String, Object> extractPropertyRegardsKeyValue(IProperty<?> regardsProp, StacProperty sp) {
-        return Tuple.of(sp.getStacPropertyName(), convertRegardsToStacValue(regardsProp, sp));
+    private Tuple2<String, Object> extractStacProperty(DataObject feature, StacProperty stacProperty) {
+        Tuple2<String, Object> tuple2 = Tuple.of(stacProperty.getStacPropertyName(),
+                                                 stacProperty.getRegardsPropertyAccessor().getGenericExtractValueFn()
+                                                         .apply(feature).map(val -> convertStacProperty(val, stacProperty))
+                                                         .getOrNull());
+        return tuple2;
     }
 
     @SuppressWarnings("unchecked")
-    private Object convertRegardsToStacValue(IProperty<?> regardsProp, StacProperty sp) {
-        return sp.getConverter()
-            .convertRegardsToStac(extractValue(regardsProp))
-            .onFailure(t -> warn(LOGGER, "Could not convert regards property {}={} to stac property {}",
-                    regardsProp.getName(),
-                    regardsProp.getValue(),
-                    sp.getStacPropertyName())
-            )
-            .getOrElse(regardsProp.getValue());
+    private Object convertStacProperty(Object value, StacProperty sp) {
+        return sp.getConverter().convertRegardsToStac(value)
+                .onFailure(t -> warn(LOGGER, "Could not convert regards property value {} using stac property {} converter",
+                                     value, sp.getStacPropertyName()))
+                .getOrElse(value);
     }
 
-    private Object extractValue(IProperty<?> regardsProp) {
-        Object value = regardsProp.getValue();
-        if (value instanceof Number) {
-            return ((Number)value).doubleValue();
-        }
-        else {
-            return value;
-        }
-    }
+    // FIXME remove
+    //    private Map<String, Object> extractStacPropertyKeyValues(DataObject feature, List<StacProperty> properties) {
+    //        return HashSet.ofAll(feature.getFeature().getProperties())
+    //                .map(regardsProp -> findCorrespondingStacProperty(regardsProp, properties)
+    //                        .map(sp -> extractPropertyRegardsKeyValue(regardsProp, sp))
+    //                        .getOrElse(() -> Tuple.of(regardsProp.getName(), regardsProp.getValue())))
+    //                .toMap(kv -> kv).filterKeys(isNotNull());
+    //    }
 
-    private Option<StacProperty> findCorrespondingStacProperty(IProperty<?> regardsProp, List<StacProperty> properties) {
-        return properties.find(sp -> {
-            String regardsAttributeName = sp.getRegardsPropertyAccessor().getRegardsAttributeName();
-            return regardsProp.getName().equals(regardsAttributeName);
-        });
-    }
+    //    private Tuple2<String, Object> extractPropertyRegardsKeyValue(IProperty<?> regardsProp, StacProperty sp) {
+    //        return Tuple.of(sp.getStacPropertyName(), convertRegardsToStacValue(regardsProp, sp));
+    //    }
+
+    //    @SuppressWarnings("unchecked")
+    //    private Object convertRegardsToStacValue(IProperty<?> regardsProp, StacProperty sp) {
+    //        return sp.getConverter().convertRegardsToStac(extractValue(regardsProp))
+    //                .onFailure(t -> warn(LOGGER, "Could not convert regards property {}={} to stac property {}",
+    //                                     regardsProp.getName(), regardsProp.getValue(), sp.getStacPropertyName()))
+    //                .getOrElse(regardsProp.getValue());
+    //    }
+
+    //    private Object extractValue(IProperty<?> regardsProp) {
+    //        Object value = regardsProp.getValue();
+    //        if (value instanceof Number) {
+    //            return ((Number) value).doubleValue();
+    //        } else {
+    //            return value;
+    //        }
+    //    }
+    //
+    //    private Option<StacProperty> findCorrespondingStacProperty(IProperty<?> regardsProp, List<StacProperty> properties) {
+    //        return properties.find(sp -> {
+    //            String regardsAttributeName = sp.getRegardsPropertyAccessor().getRegardsAttributeName();
+    //            return regardsProp.getName().equals(regardsAttributeName);
+    //        });
+    //    }
 
 }
