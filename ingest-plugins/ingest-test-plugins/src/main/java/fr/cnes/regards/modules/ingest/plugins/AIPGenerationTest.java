1816b8a5519535366d7bc3e4503b8a33bf355670
sbinda <sebastien.binda@c-s.fr>
2021-06-25 12:10:53 +0200

    Add new AIP generation plugin for tests

---
+++ ingest-plugins/ingest-test-plugins/src/main/java/fr/cnes/regards/modules/ingest/plugins/AIPGenerationTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2017-2021 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.regards.modules.ingest.plugins;
+
+import fr.cnes.regards.framework.modules.plugins.annotations.Plugin;
+import fr.cnes.regards.framework.modules.plugins.annotations.PluginParameter;
+import fr.cnes.regards.framework.oais.urn.OAISIdentifier;
+import fr.cnes.regards.framework.oais.urn.OaisUniformResourceName;
+import fr.cnes.regards.framework.urn.EntityType;
+import fr.cnes.regards.framework.utils.plugins.PluginUtilsRuntimeException;
+import fr.cnes.regards.modules.ingest.domain.exception.AIPGenerationException;
+import fr.cnes.regards.modules.ingest.domain.plugin.IAipGeneration;
+import fr.cnes.regards.modules.ingest.domain.sip.SIPEntity;
+import fr.cnes.regards.modules.ingest.dto.aip.AIP;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Test plugin class to simulate aip generation error
+ *
+ * @author Sébastien Binda
+ */
+@Plugin(author = "REGARDS Team", description = "Simple plugin to generate AIP with or without simulated errors",
+        id = "AIPGenerationTest", version = "1.0.0", contact = "regards@c-s.fr", license = "GPLv3",
+        owner = "CNES", url = "https://regardsoss.github.io/")
+public class AIPGenerationTest implements IAipGeneration  {
+
+    @PluginParameter(label = "Simulate errors", description = "If true, simulate AIP generation errors for test purpose", defaultValue = "false")
+    private Boolean simulateErrors;
+
+    @Override
+    public List<AIP> generate(SIPEntity sip, String tenant, EntityType entityType) throws AIPGenerationException {
+        if (simulateErrors) {
+            throw new AIPGenerationException(String.format("Simulated AIP generation error for sip %s",sip.getProviderId()));
+        } else {
+            List<AIP> aips = new ArrayList<>();
+            // in this case we just use SIP providerId as there is only one AIP generated, no need to tweak it
+            Integer version = sip.getVersion();
+            OaisUniformResourceName sipIdUrn = sip.getSipIdUrn();
+            aips.add(AIP.build(sip.getSip(),
+                               new OaisUniformResourceName(OAISIdentifier.AIP, entityType, tenant, sipIdUrn.getEntityId(),
+                                                           version, null, null),
+                               Optional.of(sipIdUrn), sip.getProviderId(), version));
+            return aips;
+        }
+    }
+}
