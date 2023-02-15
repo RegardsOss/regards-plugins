335d07a0425470a2059ad2caf06ddd6ee411622f
BINDA Sebastien <sebastien.binda@csgroup.eu>
2023-02-15 13:28:15 +0000

    feat(104): add new worker clean ingest post process plugin

---
+++ ingest-plugins/send-delete-files-worker-request-plugin/src/main/java/fr/cnes/regards/modules/ingest/service/plugin/SendDeleteFilesWorkerRequest.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2017-2023 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.regards.modules.ingest.service.plugin;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.gson.Gson;
+import fr.cnes.regards.framework.amqp.IPublisher;
+import fr.cnes.regards.framework.modules.plugins.annotations.Plugin;
+import fr.cnes.regards.framework.modules.plugins.annotations.PluginParameter;
+import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
+import fr.cnes.regards.framework.oais.ContentInformation;
+import fr.cnes.regards.modules.ingest.domain.aip.AIPEntity;
+import fr.cnes.regards.modules.ingest.domain.aip.AbstractAIPEntity;
+import fr.cnes.regards.modules.ingest.domain.plugin.ISipPostprocessing;
+import fr.cnes.regards.modules.ingest.domain.request.postprocessing.PostProcessResult;
+import fr.cnes.regards.modules.workermanager.amqp.events.RawMessageBuilder;
+import fr.cnes.regards.modules.workermanager.amqp.events.in.RequestEvent;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.amqp.core.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Sébastien Binda
+ **/
+@Plugin(id = SendDeleteFilesWorkerRequest.PLUGIN_ID,
+        version = "1.12.0-SNAPSHOT",
+        description = "This ingest post processing plugin send a request to worker manager with all input files"
+                + " urls in the original SIP. This plugin does not delete files itself. The deletion is done by"
+                + " the associated worker so you have to configure the associated worker in the workermanager "
+                + "microservice",
+        author = "REGARDS Team",
+        contact = "regards@c-s.fr",
+        license = "GPLv3",
+        owner = "CSSI",
+        url = "https://github.com/RegardsOss")
+public class SendDeleteFilesWorkerRequest implements ISipPostprocessing {
+
+    public final static String PLUGIN_ID = "WorkerCleanRequestPlugin";
+
+    public final static String WORKER_CONTENT_TYPE_DEFAULT = "delete-files-request";
+
+    @Autowired
+    private IPublisher publisher;
+
+    @Autowired
+    private IRuntimeTenantResolver runtimeTenantResolver;
+
+    @Autowired
+    private Gson gson;
+
+    @PluginParameter(label = "Content type",
+            description = "Select content type for the request sent to workermanager microservice",
+            optional = true,
+            defaultValue = WORKER_CONTENT_TYPE_DEFAULT)
+    private String contentType = WORKER_CONTENT_TYPE_DEFAULT;
+
+    @Override
+    public PostProcessResult postprocess(Collection<AIPEntity> aipEntities) {
+        PostProcessResult ppr = new PostProcessResult();
+        ppr.buildErrors(Maps.newHashMap());
+        if (!CollectionUtils.isEmpty(aipEntities)) {
+            computeFilesToDeletePerSession(aipEntities).asMap().forEach((session, urls) -> {
+                Message message = RawMessageBuilder.build(runtimeTenantResolver.getTenant(),
+                        contentType,
+                        session.getLeft(),
+                        session.getRight(),
+                        UUID.randomUUID().toString(),
+                        urls,
+                        gson);
+
+                publisher.basicPublish(runtimeTenantResolver.getTenant(), "regards.broadcast." + RequestEvent.class.getName(),
+                        "", message);
+            });
+            ppr.buildSuccesses(aipEntities.stream().map(AbstractAIPEntity::getAipId).collect(Collectors.toSet()));
+        }
+
+        return ppr;
+    }
+
+    private Multimap<Pair<String, String>, String> computeFilesToDeletePerSession(Collection<AIPEntity> aipEntities) {
+        Multimap<Pair<String, String>, String> entitiesPerSession = ArrayListMultimap.create();
+        aipEntities.stream().forEach(a -> {
+            Pair<String, String> session = Pair.of(a.getSip().getSessionOwner(), a.getSip().getSession());
+            entitiesPerSession.putAll(session, getUrlsToDelete(a));
+        });
+        return entitiesPerSession;
+    }
+
+    private List<String> getUrlsToDelete(AIPEntity aipEntity) {
+        return aipEntity.getSip()
+                .getSip()
+                .getProperties()
+                .getContentInformations()
+                .stream()
+                .flatMap(this::getUrlsToDeleteFromDataObject)
+                .toList();
+    }
+
+    private Stream<String> getUrlsToDeleteFromDataObject(ContentInformation contentInformation) {
+        return contentInformation.getDataObject()
+                .getLocations()
+                .stream()
+                .filter(loc -> loc.getStorage() == null)
+                .map(f -> f.getUrl());
+    }
+
+}
---
+++ ingest-plugins/send-delete-files-worker-request-plugin/src/test/java/fr/cnes/regards/modules/ingest/service/plugin/SendDeleteFilesWorkerRequestTest.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2017-2023 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.regards.modules.ingest.service.plugin;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import fr.cnes.regards.framework.amqp.IPublisher;
+import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
+import fr.cnes.regards.framework.oais.InformationPackageProperties;
+import fr.cnes.regards.framework.oais.OAISDataObjectLocation;
+import fr.cnes.regards.framework.urn.DataType;
+import fr.cnes.regards.framework.urn.EntityType;
+import fr.cnes.regards.modules.ingest.domain.aip.AIPEntity;
+import fr.cnes.regards.modules.ingest.domain.sip.SIPEntity;
+import fr.cnes.regards.modules.ingest.dto.sip.SIP;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.amqp.core.Message;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author Sébastien Binda
+ **/
+public class SendDeleteFilesWorkerRequestTest {
+
+    private IPublisher publisher;
+
+    private IRuntimeTenantResolver runtimeTenantResolver;
+
+    private SendDeleteFilesWorkerRequest plugin;
+
+    private ArgumentCaptor<Message> messageCaptor;
+
+    private Gson gson;
+
+    @Before
+    public void init() {
+        gson = new GsonBuilder().create();
+        publisher = Mockito.mock(IPublisher.class);
+        runtimeTenantResolver = Mockito.mock(IRuntimeTenantResolver.class);
+
+        plugin = new SendDeleteFilesWorkerRequest();
+        ReflectionTestUtils.setField(plugin, "publisher", publisher);
+        ReflectionTestUtils.setField(plugin, "runtimeTenantResolver", runtimeTenantResolver);
+        ReflectionTestUtils.setField(plugin, "gson", new GsonBuilder().create());
+        ReflectionTestUtils.setField(plugin, "contentType", SendDeleteFilesWorkerRequest.WORKER_CONTENT_TYPE_DEFAULT);
+
+        messageCaptor = ArgumentCaptor.forClass(Message.class);
+    }
+
+    @Test
+    public void test_urls_in_worker_request_from_aips() {
+
+        // GIVEN
+        String url1 = "file:///test/tmp/file.txt";
+        String url2 = "http://where/test/tmp/file.txt";
+        String url3 = "http://where/test/tmp/file2.txt";
+        String url4 = "file:///test/tmp/file2.txt";
+        Collection<AIPEntity> aipEntities = List.of(initEntity("001", "owner1", "session1", url1),
+                initEntity("002", "owner1", "session1", url2),
+                initEntity("003", "owner1", "session1", url3, url4));
+
+        // WHEN
+        plugin.postprocess(aipEntities);
+
+        // THEN
+        Mockito.verify(publisher, Mockito.times(1))
+                .basicPublish(Mockito.any(),
+                        Mockito.any(),
+                        Mockito.any(),
+                        messageCaptor.capture());
+
+        List<String> urls = deserializeMessages();
+        Assert.assertEquals(4, urls.size());
+        Assert.assertTrue(urls.contains(url1));
+        Assert.assertTrue(urls.contains(url2));
+        Assert.assertTrue(urls.contains(url3));
+        Assert.assertTrue(urls.contains(url4));
+
+    }
+
+    @Test
+    public void test_urls_in_worker_request_from_aips_many_sessions() {
+
+        // GIVEN
+        String url1 = "file:///test/tmp/file.txt";
+        String url2 = "http://where/test/tmp/file.txt";
+        String url3 = "http://where/test/tmp/file2.txt";
+        String url4 = "file:///test/tmp/file2.txt";
+        Collection<AIPEntity> aipEntities = List.of(initEntity("001", "owner1", "session1", url1),
+                initEntity("002", "owner1", "session2", url2),
+                initEntity("003", "owner2", "session1", url3, url4));
+
+        // WHEN
+        plugin.postprocess(aipEntities);
+
+        // THEN
+        Mockito.verify(publisher, Mockito.times(3))
+                .basicPublish(Mockito.any(),
+                        Mockito.any(),
+                        Mockito.any(),
+                        messageCaptor.capture());
+
+        List<String> urls = deserializeMessages();
+        Assert.assertEquals(4, urls.size());
+        Assert.assertTrue(urls.contains(url1));
+        Assert.assertTrue(urls.contains(url2));
+        Assert.assertTrue(urls.contains(url3));
+        Assert.assertTrue(urls.contains(url4));
+
+    }
+
+    private List<String> deserializeMessages() {
+        List<String> results = new ArrayList<>();
+        messageCaptor.getAllValues().stream().forEach(message -> results.addAll((List<String>) gson.fromJson(new String(message.getBody(),
+                        StandardCharsets.UTF_8),
+                List.class)));
+        return results;
+    }
+
+    private AIPEntity initEntity(String providerId, String owner, String session, String... urls) {
+        AIPEntity aipEntity = new AIPEntity();
+        SIPEntity sipEntity = new SIPEntity();
+        SIP sip = SIP.build(EntityType.DATA, providerId);
+        InformationPackageProperties ip = InformationPackageProperties.build();
+        OAISDataObjectLocation[] locs = Arrays.stream(urls)
+                .map(OAISDataObjectLocation::build)
+                .toList()
+                .toArray(new OAISDataObjectLocation[urls.length]);
+        ip.withDataObject(DataType.RAWDATA, "filename", "MD5", "123456789", 12L, locs);
+        ip.registerContentInformation();
+        sip.setProperties(ip);
+        sipEntity.setSession(session);
+        sipEntity.setSessionOwner(owner);
+        sipEntity.setSip(sip);
+        aipEntity.setSip(sipEntity);
+        return aipEntity;
+    }
+
+}
