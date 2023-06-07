c6f105844f79ffefb3945ec29adbcb8311f10c05
Sebastien Binda <sebastien.binda@csgroup.eu>
2023-06-07 14:29:50 +0200

    fix(sonar): fix sonar rules

--- catalog-plugins/stac/stac-plugin/src/main/java/fr/cnes/regards/modules/catalog/stac/plugin/configuration/mapping/ConfigurationAccessorFactoryImpl.java
+++ catalog-plugins/stac/stac-plugin/src/main/java/fr/cnes/regards/modules/catalog/stac/plugin/configuration/mapping/ConfigurationAccessorFactoryImpl.java
@@ -21,7 +21,6 @@ package fr.cnes.regards.modules.catalog.stac.plugin.configuration.mapping;
 
 import fr.cnes.regards.framework.modules.plugins.service.IPluginService;
 import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
-import fr.cnes.regards.modules.catalog.stac.domain.StacSpecConstants;
 import fr.cnes.regards.modules.catalog.stac.domain.properties.StacProperty;
 import fr.cnes.regards.modules.catalog.stac.domain.properties.StacPropertyType;
 import fr.cnes.regards.modules.catalog.stac.domain.properties.conversion.AbstractPropertyConverter;
@@ -29,16 +28,14 @@ import fr.cnes.regards.modules.catalog.stac.domain.properties.conversion.Propert
 import fr.cnes.regards.modules.catalog.stac.domain.spec.v1_0_0_beta2.collection.Provider;
 import fr.cnes.regards.modules.catalog.stac.domain.spec.v1_0_0_beta2.collection.Provider.ProviderRole;
 import fr.cnes.regards.modules.catalog.stac.plugin.StacSearchEngine;
-import fr.cnes.regards.modules.catalog.stac.plugin.configuration.*;
+import fr.cnes.regards.modules.catalog.stac.plugin.configuration.ProviderConfiguration;
+import fr.cnes.regards.modules.catalog.stac.plugin.configuration.StacPropertyConfiguration;
+import fr.cnes.regards.modules.catalog.stac.plugin.configuration.StacSourcePropertyConfiguration;
 import fr.cnes.regards.modules.catalog.stac.service.configuration.ConfigurationAccessor;
 import fr.cnes.regards.modules.catalog.stac.service.configuration.ConfigurationAccessorFactory;
 import fr.cnes.regards.modules.indexer.dao.spatial.ProjectGeoSettings;
 import io.vavr.collection.List;
 import io.vavr.control.Option;
-import io.vavr.control.Try;
-import org.locationtech.spatial4j.context.jts.JtsSpatialContext;
-import org.locationtech.spatial4j.context.jts.JtsSpatialContextFactory;
-import org.locationtech.spatial4j.io.GeoJSONReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,7 +54,7 @@ import static fr.cnes.regards.modules.catalog.stac.domain.utils.TryDSL.trying;
  */
 @Component
 public class ConfigurationAccessorFactoryImpl extends AbstractConfigurationAccessor
-    implements ConfigurationAccessorFactory {
+        implements ConfigurationAccessorFactory {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationAccessorFactoryImpl.class);
 
@@ -79,157 +76,45 @@ public class ConfigurationAccessorFactoryImpl extends AbstractConfigurationAcces
         this.geoSettings = geoSettings;
     }
 
-    @Override
-    public ConfigurationAccessor makeConfigurationAccessor() {
-        final Try<StacSearchEngine> plugin = getPlugin(StacSearchEngine.PLUGIN_ID);
-        return new ConfigurationAccessor() {
-
-            @Override
-            public String getTitle() {
-                return plugin.map(StacSearchEngine::getStacTitle).getOrElse(() -> {
-                    String tenant = runtimeTenantResolver.getTenant();
-                    return "STAC Catalog " + tenant;
-                });
-            }
-
-            @Override
-            public String getDescription() {
-                return plugin.map(StacSearchEngine::getStacDescription).getOrElse(() -> {
-                    String tenant = runtimeTenantResolver.getTenant();
-                    return "STAC Catalog " + tenant;
-                });
-            }
-
-            @Override
-            public List<StacProperty> getStacProperties() {
-                return plugin.map(ConfigurationAccessorFactoryImpl.this::getConfiguredProperties)
-                             .getOrElse(List.empty());
-            }
-
-            @Override
-            public StacProperty getDatetimeStacProperty() {
-                return plugin.map(p -> p.getStacDatetimeProperty().toStacPropertyConfiguration())
-                             .map(spc -> getConfiguredProperties(List.of(spc)).head())
-                             .getOrNull();
-            }
-
-            @Override
-            public StacProperty getLinksStacProperty() {
-                String stacPropertyName = StacSpecConstants.PropertyName.STAC_LINKS_PROPERTY_NAME;
-                return plugin.map(p -> makeStacProperty(p.getStacLinksProperty(),
-                                                        stacPropertyName,
-                                                        StacPropertyType.JSON_OBJECT))
-                             .getOrElse(makeDefaultStacProperty(StacSpecConstants.SourcePropertyName.STAC_LINKS_SOURCE_PROPERTY_NAME,
-                                                                stacPropertyName,
-                                                                StacPropertyType.JSON_OBJECT));
-            }
-
-            @Override
-            public StacProperty getAssetsStacProperty() {
-                String stacPropertyName = StacSpecConstants.PropertyName.STAC_ASSETS_PROPERTY_NAME;
-                return plugin.map(p -> makeStacProperty(p.getStacAssetsProperty(),
-                                                        stacPropertyName,
-                                                        StacPropertyType.JSON_OBJECT))
-                             .getOrElse(makeDefaultStacProperty(StacSpecConstants.SourcePropertyName.STAC_ASSETS_SOURCE_PROPERTY_NAME,
-                                                                stacPropertyName,
-                                                                StacPropertyType.JSON_OBJECT));
-            }
-
-            @Override
-            public List<Provider> getProviders(String datasetUrn) {
-                return getCollectionConfigs(datasetUrn).flatMap(CollectionConfiguration::getProviders)
-                                                       .map(pc -> getProvider(pc));
-            }
-
-            @Override
-            public List<String> getKeywords(String datasetUrn) {
-                return getCollectionConfigs(datasetUrn).flatMap(cc -> List.ofAll(cc.getKeywords()));
-            }
-
-            @Override
-            public String getLicense(String datasetUrn) {
-                return getCollectionConfigs(datasetUrn).headOption()
-                                                       .map(CollectionConfiguration::getLicense)
-                                                       .getOrNull();
-            }
-
-            private List<CollectionConfiguration> getCollectionConfigs(String datasetUrn) {
-                return plugin.map(StacSearchEngine::getStacCollectionDatasetProperties)
-                             .map(List::ofAll)
-                             .getOrElse(List.empty())
-                             .filter(cc -> cc.getDatasetUrns().contains(datasetUrn));
-            }
-
-            @Override
-            public GeoJSONReader getGeoJSONReader() {
-                JtsSpatialContextFactory factory = new JtsSpatialContextFactory();
-                factory.geo = geoSettings.getShouldManagePolesOnGeometries();
-                return new GeoJSONReader(new JtsSpatialContext(factory), factory);
-            }
-
-            @Override
-            public String getRootDynamicCollectionName() {
-                return plugin.map(StacSearchEngine::getRootDynamicCollectionTitle).getOrElse("dynamic");
-            }
-
-            @Override
-            public String getRootStaticCollectionName() {
-                return plugin.map(StacSearchEngine::getRootStaticCollectionTitle).getOrElse("static");
-            }
-
-            @Override
-            public String getEODAGPortalName() {
-                return plugin.map(StacSearchEngine::getEodagConfiguration)
-                             .map(EODAGConfiguration::getPortalName)
-                             .getOrElse(() -> getTitle());
-            }
-
-            @Override
-            public String getEODAGProvider() {
-                return plugin.map(StacSearchEngine::getEodagConfiguration)
-                             .map(EODAGConfiguration::getProvider)
-                             .getOrElse("provider");
-            }
-
-            @Override
-            public String getEODAGApiKey() {
-                return plugin.map(StacSearchEngine::getEodagConfiguration)
-                             .map(EODAGConfiguration::getApiKey)
-                             .getOrElse("apiKey");
-            }
-        };
+    public ProjectGeoSettings getGeoSettings() {
+        return geoSettings;
     }
 
-    private Provider getProvider(ProviderConfiguration pc) {
+    @Override
+    public ConfigurationAccessor makeConfigurationAccessor() {
+        return new CustomConfigurationAccessor(runtimeTenantResolver, getPlugin(StacSearchEngine.PLUGIN_ID), this);
+    }
+
+    public Provider getProvider(ProviderConfiguration pc) {
         List<ProviderRole> roles = List.ofAll(pc.getProviderRoles())
-                                       .flatMap(role -> trying(() -> ProviderRole.valueOf(role)).onFailure(t -> warn(
-                                           LOGGER,
-                                           "Failed to parse provider role {}",
-                                           role)));
+                .flatMap(role -> trying(() -> ProviderRole.valueOf(role)).onFailure(t -> warn(
+                        LOGGER,
+                        "Failed to parse provider role {}",
+                        role)));
         URL providerUrl = trying(() -> new URL(pc.getProviderUrl())).onFailure(t -> warn(LOGGER,
-                                                                                         "Failed to parse provider URL in {}",
-                                                                                         pc)).getOrNull();
+                "Failed to parse provider URL in {}",
+                pc)).getOrNull();
         return new Provider(pc.getProviderName(), pc.getProviderDescription(), providerUrl, roles);
     }
 
-    private List<StacProperty> getConfiguredProperties(List<StacPropertyConfiguration> paramConfigurations) {
+    public List<StacProperty> getConfiguredProperties(List<StacPropertyConfiguration> paramConfigurations) {
         return paramConfigurations.map(s -> {
             trace(LOGGER, "Converting stac prop config: {}", s);
             StacPropertyType stacType = StacPropertyType.parse(s.getStacPropertyType());
             @SuppressWarnings("rawtypes") AbstractPropertyConverter converter = propertyConverterFactory.getConverter(
-                stacType,
-                s.getStacPropertyFormat(),
-                s.getSourcePropertyFormat());
+                    stacType,
+                    s.getStacPropertyFormat(),
+                    s.getSourcePropertyFormat());
             return new StacProperty(extractPropertyAccessor(s, stacType),
-                                    s.getStacPropertyNamespace(),
-                                    s.getStacPropertyName(),
-                                    s.getStacPropertyExtension(),
-                                    s.getStacComputeSummary() && canComputeSummary(stacType),
-                                    s.getStacDynamicCollectionLevel(),
-                                    s.getStacDynamicCollectionFormat(),
-                                    stacType,
-                                    converter,
-                                    Boolean.FALSE);
+                    s.getStacPropertyNamespace(),
+                    s.getStacPropertyName(),
+                    s.getStacPropertyExtension(),
+                    s.getStacComputeSummary() && canComputeSummary(stacType),
+                    s.getStacDynamicCollectionLevel(),
+                    s.getStacDynamicCollectionFormat(),
+                    stacType,
+                    converter,
+                    Boolean.FALSE);
         }).toList();
     }
 
@@ -237,44 +122,44 @@ public class ConfigurationAccessorFactoryImpl extends AbstractConfigurationAcces
         return type.canBeSummarized();
     }
 
-    private List<StacProperty> getConfiguredProperties(StacSearchEngine plugin) {
+    public List<StacProperty> getConfiguredProperties(StacSearchEngine plugin) {
         java.util.List<StacPropertyConfiguration> propConfigs = Option.of(plugin.getStacExtraProperties())
-                                                                      .getOrElse(new ArrayList<>());
+                .getOrElse(new ArrayList<>());
         StacPropertyConfiguration datetimeProp = plugin.getStacDatetimeProperty().toStacPropertyConfiguration();
         return addVirtualStacProperties(getConfiguredProperties(List.ofAll(propConfigs).prepend(datetimeProp)));
     }
 
-    private StacProperty makeStacProperty(StacSourcePropertyConfiguration sourcePropertyConfiguration,
-                                          String stacPropertyName,
-                                          StacPropertyType stacType) {
+    public StacProperty makeStacProperty(StacSourcePropertyConfiguration sourcePropertyConfiguration,
+                                         String stacPropertyName,
+                                         StacPropertyType stacType) {
         return new StacProperty(extractPropertyAccessor(sourcePropertyConfiguration, stacType),
-                                null,
-                                stacPropertyName,
-                                null,
-                                false,
-                                null,
-                                null,
-                                stacType,
-                                null,
-                                Boolean.FALSE);
+                null,
+                stacPropertyName,
+                null,
+                false,
+                null,
+                null,
+                stacType,
+                null,
+                Boolean.FALSE);
     }
 
-    private StacProperty makeDefaultStacProperty(String sourcePropertyPath,
-                                                 String stacPropertyName,
-                                                 StacPropertyType stacType) {
+    public StacProperty makeDefaultStacProperty(String sourcePropertyPath,
+                                                String stacPropertyName,
+                                                StacPropertyType stacType) {
         StacSourcePropertyConfiguration sourcePropertyConfiguration = new StacSourcePropertyConfiguration(
-            sourcePropertyPath,
-            null,
-            null);
+                sourcePropertyPath,
+                null,
+                null);
         return new StacProperty(extractPropertyAccessor(sourcePropertyConfiguration, stacType),
-                                null,
-                                stacPropertyName,
-                                null,
-                                false,
-                                null,
-                                null,
-                                stacType,
-                                null,
-                                Boolean.FALSE);
+                null,
+                stacPropertyName,
+                null,
+                false,
+                null,
+                null,
+                stacType,
+                null,
+                Boolean.FALSE);
     }
 }
---
+++ catalog-plugins/stac/stac-plugin/src/main/java/fr/cnes/regards/modules/catalog/stac/plugin/configuration/mapping/CustomConfigurationAccessor.java
@@ -0,0 +1,170 @@
+/*
+ * Copyright 2017-2023 CNES - CENTRE NATIONAL d'ETUDES SPATIALES
+ *
+ * This file is part of REGARDS.
+ *
+ * REGARDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * REGARDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+package fr.cnes.regards.modules.catalog.stac.plugin.configuration.mapping;
+
+import fr.cnes.regards.framework.multitenant.IRuntimeTenantResolver;
+import fr.cnes.regards.modules.catalog.stac.domain.StacSpecConstants;
+import fr.cnes.regards.modules.catalog.stac.domain.properties.StacProperty;
+import fr.cnes.regards.modules.catalog.stac.domain.properties.StacPropertyType;
+import fr.cnes.regards.modules.catalog.stac.domain.spec.v1_0_0_beta2.collection.Provider;
+import fr.cnes.regards.modules.catalog.stac.plugin.StacSearchEngine;
+import fr.cnes.regards.modules.catalog.stac.plugin.configuration.CollectionConfiguration;
+import fr.cnes.regards.modules.catalog.stac.plugin.configuration.EODAGConfiguration;
+import fr.cnes.regards.modules.catalog.stac.service.configuration.ConfigurationAccessor;
+import io.vavr.collection.List;
+import io.vavr.control.Try;
+import org.locationtech.spatial4j.context.jts.JtsSpatialContext;
+import org.locationtech.spatial4j.context.jts.JtsSpatialContextFactory;
+import org.locationtech.spatial4j.io.GeoJSONReader;
+
+/**
+ * Provides access to the values present in the STAC plugin configuration.
+ *
+ * @author Sébastien Binda
+ **/
+public class CustomConfigurationAccessor implements ConfigurationAccessor {
+
+    private final IRuntimeTenantResolver runtimeTenantResolver;
+
+    private final Try<StacSearchEngine> plugin;
+
+    private final ConfigurationAccessorFactoryImpl configurationAccessorFactoryImpl;
+
+
+    public CustomConfigurationAccessor(IRuntimeTenantResolver runtimeTenantResolver, Try<StacSearchEngine> plugin, ConfigurationAccessorFactoryImpl configurationAccessorFactoryImpl) {
+        this.runtimeTenantResolver = runtimeTenantResolver;
+        this.plugin = plugin;
+        this.configurationAccessorFactoryImpl = configurationAccessorFactoryImpl;
+    }
+
+    @Override
+    public String getTitle() {
+        return plugin.map(StacSearchEngine::getStacTitle).getOrElse(() -> {
+            String tenant = runtimeTenantResolver.getTenant();
+            return "STAC Catalog " + tenant;
+        });
+    }
+
+    @Override
+    public String getDescription() {
+        return plugin.map(StacSearchEngine::getStacDescription).getOrElse(() -> {
+            String tenant = runtimeTenantResolver.getTenant();
+            return "STAC Catalog " + tenant;
+        });
+    }
+
+    @Override
+    public List<StacProperty> getStacProperties() {
+        return plugin.map(configurationAccessorFactoryImpl::getConfiguredProperties)
+                .getOrElse(List.empty());
+    }
+
+    @Override
+    public StacProperty getDatetimeStacProperty() {
+        return plugin.map(p -> p.getStacDatetimeProperty().toStacPropertyConfiguration())
+                .map(spc -> configurationAccessorFactoryImpl.getConfiguredProperties(List.of(spc)).head())
+                .getOrNull();
+    }
+
+    @Override
+    public StacProperty getLinksStacProperty() {
+        String stacPropertyName = StacSpecConstants.PropertyName.STAC_LINKS_PROPERTY_NAME;
+        return plugin.map(p -> configurationAccessorFactoryImpl.makeStacProperty(p.getStacLinksProperty(),
+                        stacPropertyName,
+                        StacPropertyType.JSON_OBJECT))
+                .getOrElse(configurationAccessorFactoryImpl.makeDefaultStacProperty(StacSpecConstants.SourcePropertyName.STAC_LINKS_SOURCE_PROPERTY_NAME,
+                        stacPropertyName,
+                        StacPropertyType.JSON_OBJECT));
+    }
+
+    @Override
+    public StacProperty getAssetsStacProperty() {
+        String stacPropertyName = StacSpecConstants.PropertyName.STAC_ASSETS_PROPERTY_NAME;
+        return plugin.map(p -> configurationAccessorFactoryImpl.makeStacProperty(p.getStacAssetsProperty(),
+                        stacPropertyName,
+                        StacPropertyType.JSON_OBJECT))
+                .getOrElse(configurationAccessorFactoryImpl.makeDefaultStacProperty(StacSpecConstants.SourcePropertyName.STAC_ASSETS_SOURCE_PROPERTY_NAME,
+                        stacPropertyName,
+                        StacPropertyType.JSON_OBJECT));
+    }
+
+    @Override
+    public List<Provider> getProviders(String datasetUrn) {
+        return getCollectionConfigs(datasetUrn).flatMap(CollectionConfiguration::getProviders)
+                .map(configurationAccessorFactoryImpl::getProvider);
+    }
+
+    @Override
+    public List<String> getKeywords(String datasetUrn) {
+        return getCollectionConfigs(datasetUrn).flatMap(cc -> List.ofAll(cc.getKeywords()));
+    }
+
+    @Override
+    public String getLicense(String datasetUrn) {
+        return getCollectionConfigs(datasetUrn).headOption()
+                .map(CollectionConfiguration::getLicense)
+                .getOrNull();
+    }
+
+    private List<CollectionConfiguration> getCollectionConfigs(String datasetUrn) {
+        return plugin.map(StacSearchEngine::getStacCollectionDatasetProperties)
+                .map(List::ofAll)
+                .getOrElse(List.empty())
+                .filter(cc -> cc.getDatasetUrns().contains(datasetUrn));
+    }
+
+    @Override
+    public GeoJSONReader getGeoJSONReader() {
+        JtsSpatialContextFactory factory = new JtsSpatialContextFactory();
+        factory.geo = configurationAccessorFactoryImpl.getGeoSettings().getShouldManagePolesOnGeometries();
+        return new GeoJSONReader(new JtsSpatialContext(factory), factory);
+    }
+
+    @Override
+    public String getRootDynamicCollectionName() {
+        return plugin.map(StacSearchEngine::getRootDynamicCollectionTitle).getOrElse("dynamic");
+    }
+
+    @Override
+    public String getRootStaticCollectionName() {
+        return plugin.map(StacSearchEngine::getRootStaticCollectionTitle).getOrElse("static");
+    }
+
+    @Override
+    public String getEODAGPortalName() {
+        return plugin.map(StacSearchEngine::getEodagConfiguration)
+                .map(EODAGConfiguration::getPortalName)
+                .getOrElse(this::getTitle);
+    }
+
+    @Override
+    public String getEODAGProvider() {
+        return plugin.map(StacSearchEngine::getEodagConfiguration)
+                .map(EODAGConfiguration::getProvider)
+                .getOrElse("provider");
+    }
+
+    @Override
+    public String getEODAGApiKey() {
+        return plugin.map(StacSearchEngine::getEodagConfiguration)
+                .map(EODAGConfiguration::getApiKey)
+                .getOrElse("apiKey");
+    }
+
+}
