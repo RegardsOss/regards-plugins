a65a4bef1a3ac2ff282fce3cb617004764fe9b03
Thibaud M <thibaud.michaudel@csgroup.eu>
2023-12-11 15:39:36 +0100

    fix(neo): fix following 1.14.1 merge

--- dam-plugins/feature-datasource/src/test/java/fr/cnes/regards/modules/dam/plugins/datasources/FeatureDatasourcePluginTest.java
+++ dam-plugins/feature-datasource/src/test/java/fr/cnes/regards/modules/dam/plugins/datasources/FeatureDatasourcePluginTest.java
@@ -19,7 +19,7 @@
 package fr.cnes.regards.modules.dam.plugins.datasources;
 
 import fr.cnes.regards.framework.modules.plugins.domain.PluginConfiguration;
-import fr.cnes.regards.framework.modules.plugins.domain.parameter.IPluginParam;
+import fr.cnes.regards.framework.modules.plugins.dto.parameter.parameter.IPluginParam;
 import fr.cnes.regards.framework.utils.plugins.PluginUtils;
 import fr.cnes.regards.framework.utils.plugins.bean.PluginUtilsBean;
 import fr.cnes.regards.framework.utils.plugins.exception.NotAvailablePluginConfigurationException;
