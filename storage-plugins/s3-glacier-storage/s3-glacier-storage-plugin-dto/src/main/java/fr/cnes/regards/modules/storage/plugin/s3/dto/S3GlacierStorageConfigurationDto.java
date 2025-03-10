ef5d24cbac53687722c04aec2119ecf83ae058a1
Thibaud M <thibaud.michaudel@csgroup.eu>
2025-03-10 11:14:05 +0100

    fix(s3): fix wrong package rename in merge

--- storage-plugins/s3-glacier-storage/s3-glacier-storage-plugin-dto/src/main/java/fr/cnes/regards/modules/storage/plugin/s3/dto/S3GlacierStorageConfigurationDto.java
+++ storage-plugins/s3-glacier-storage/s3-glacier-storage-plugin-dto/src/main/java/fr/cnes/regards/modules/storage/plugin/s3/dto/S3GlacierStorageConfigurationDto.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with REGARDS. If not, see <http://www.gnu.org/licenses/>.
  */
-package fr.cnes.regards.modules.storage.plugin.smallfiles.dto;
+package fr.cnes.regards.modules.storage.plugin.s3.dto;
 
 import fr.cnes.regards.framework.s3.dto.StorageConfigDto;
 import fr.cnes.regards.modules.fileaccess.dto.output.worker.FileNamingStrategy;
--- storage-plugins/s3-storage/s3-storage-plugin/src/main/java/fr/cnes/regards/modules/storage/plugin/s3/S3OnlineStorage.java
+++ storage-plugins/s3-storage/s3-storage-plugin/src/main/java/fr/cnes/regards/modules/storage/plugin/s3/S3OnlineStorage.java
@@ -9,7 +9,7 @@ import fr.cnes.regards.framework.utils.file.DownloadUtils;
 import fr.cnes.regards.modules.fileaccess.dto.FileReferenceWithoutOwnersDto;
 import fr.cnes.regards.modules.fileaccess.dto.output.worker.FileNamingStrategy;
 import fr.cnes.regards.modules.fileaccess.plugin.domain.*;
-import fr.cnes.regards.modules.storage.plugin.smallfiles.dto.S3StorageConfigurationDTO;
+import fr.cnes.regards.modules.storage.plugin.s3.dto.S3StorageConfigurationDTO;
 import fr.cnes.regards.modules.storage.s3.common.AbstractS3Storage;
 import fr.cnes.regards.modules.storage.s3.common.dto.AbstractS3ConfigurationDto;
 
